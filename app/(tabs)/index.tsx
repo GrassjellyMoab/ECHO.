155635e0e5190ea68a570f7a7ed3c01ac24da6fd
Rachel Tan <racheltanmz@gmail.com>
2025-06-05 00:25:48 +0800

    Updated file imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
+import { AppHeader } from '@/src/components/ui/AppHeader';
+import { IconSymbol } from '@/src/components/ui/IconSymbol';
 import { FirebaseImageData, useImagesStore } from '@/src/store/imgStore';
-import { AppHeader } from '@components/ui/AppHeader';
-import { IconSymbol } from '@components/ui/IconSymbol';
 import React from 'react';
 import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
